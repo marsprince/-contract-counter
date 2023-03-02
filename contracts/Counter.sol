83602afea0d4491abaf2b4311aee8893fd3b9330
liujia02 <bit_liujia@163.com>
2023-03-02 14:34:57 +0800

    deploy-main.js

---
+++ contracts/Counter.sol
@@ -0,0 +1,16 @@
+pragma solidity ^0.8.0;
+
+contract Counter {
+  uint public count = 1;
+
+  function get() public view returns (uint) {
+    return count;
+  }
+
+  function inc() public {
+    count += 1;
+  }
+  function dec() public {
+    count += 1;
+  }
+}
