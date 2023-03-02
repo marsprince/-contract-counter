4b7a51c14486228d41867631521d4df98b3b6968
liujia02 <bit_liujia@163.com>
2023-03-02 14:05:13 +0800

    init network

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,4 +3,12 @@ require("@nomicfoundation/hardhat-toolbox");
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.17",
+  harmonytest: {
+    url: "https://api.s0.b.hmny.io",
+    accounts: [process.env.PRIVATEKEYS]
+  },
+  harmony: {
+    url: "https://api.harmony.one",
+    accounts: [process.env.PRIVATEKEYS]
+  }
 };
